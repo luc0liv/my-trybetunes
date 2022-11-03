0edb1ec98c75289985338ed4da1ca807e12deeab
luc0liv <luoliveiradacunha@gmail.com>
2022-11-03 17:52:50 -0300

    REQ 2: 1/4

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,51 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  state = {
+    name: '',
+    isButtonDisabled: true,
+  };
+
+  handleChange = ({ target }) => {
+    const MIN_CHARACTERS = 3;
+    this.setState({
+      name: target.value,
+    }, () => {
+      this.setState((prevState) => ({
+        isButtonDisabled: prevState.name.length <= MIN_CHARACTERS,
+      }));
+    });
+  };
+
   render() {
+    const { name, isButtonDisabled } = this.state;
     return (
-      <h1 data-testid="page-login">Login</h1>
+      <section data-testid="page-login">
+        <h1>Login</h1>
+        <form>
+          <input
+            type="text"
+            data-testid="login-name-input"
+            value={ name }
+            onChange={ this.handleChange }
+          />
+          <button
+            type="button"
+            data-testid="login-submit-button"
+            disabled={ isButtonDisabled }
+          >
+            Entrar
+          </button>
+        </form>
+      </section>
     );
   }
 }
+
+// Login.propTypes = {
+//   name: PropTypes.string.isRequired,
+//   isButtonDisabled: PropTypes.bool.isRequired,
+//   handleChange: PropTypes.func.isRequired,
+// };
 
 export default Login;
