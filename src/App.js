a5a55c344aebf46c655d0404d770ea5866cd3736
luc0liv <luoliveiradacunha@gmail.com>
2022-11-03 18:42:15 -0300

    REQ 2:  4/4

--- src/App.js
+++ src/App.js
@@ -9,34 +9,15 @@ import ProfileEdit from './pages/ProfileEdit';
 import Search from './pages/Search';
 
 class App extends React.Component {
-  // state = {
-  //   name: '',
-  //   isButtonDisabled: true,
-  // };
-
-  // handleChange = ({ target }) => {
-  //   console.log(target.value);
-  // };
-
   render() {
-    // const { name, isButtonDisabled } = this.state;
     return (
       <Switch>
-        <Route exact path="/profile/edit" component={ ProfileEdit } />
-        <Route exact path="/profile" component={ Profile } />
-        <Route exact path="/album/:id" component={ Album } />
-        <Route exact path="/favorites" component={ Favorites } />
-        <Route exact path="/search" component={ Search } />
-        {/* <Route
-          exact
-          path="/"
-          render={ () => (<Login
-            name={ name }
-            isButtonDisabled={ isButtonDisabled }
-            handleChange={ this.handleChange }
-          />) }
-        /> */}
-        <Route path="/" component={ Login } />
+        <Route path="/profile/edit" component={ ProfileEdit } />
+        <Route path="/profile" component={ Profile } />
+        <Route path="/album/:id" component={ Album } />
+        <Route path="/favorites" component={ Favorites } />
+        <Route path="/search" component={ Search } />
+        <Route exact path="/" component={ Login } />
         <Route path="*" component={ PageNotFound } />
       </Switch>
     );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { createUser } from '../services/userAPI';
 
 class Login extends Component {
   state = {
     name: '',
     isButtonDisabled: true,
+    loading: false,
   };
 
   handleChange = ({ target }) => {
     const MIN_CHARACTERS = 3;
+    this.setState(
+      {
+        name: target.value,
+      },
+      () => {
+        this.setState((prevState) => ({
+          isButtonDisabled: prevState.name.length < MIN_CHARACTERS,
+        }));
+      },
+    );
+  };
+
+  createUserRequest = async (name) => {
+    const { history } = this.props;
     this.setState({
-      name: target.value,
-    }, () => {
-      this.setState((prevState) => ({
-        isButtonDisabled: prevState.name.length < MIN_CHARACTERS,
-      }));
+      loading: true,
     });
+    await createUser({ name });
+    this.setState({
+      loading: false,
+    });
+    history.push('/search');
   };
 
   render() {
-    const { name, isButtonDisabled } = this.state;
+    const { name, isButtonDisabled, loading } = this.state;
+    const loadingElement = <p>Carregando...</p>;
     return (
       <section data-testid="page-login">
         <h1>Login</h1>
@@ -35,20 +53,21 @@ class Login extends Component {
             type="button"
             data-testid="login-submit-button"
             disabled={ isButtonDisabled }
-            onClick={ () => createUser({ name }) }
+            onClick={ () => this.createUserRequest(name) }
           >
             Entrar
           </button>
         </form>
+        {loading && loadingElement }
       </section>
     );
   }
 }
 
-// Login.propTypes = {
-//   name: PropTypes.string.isRequired,
-//   isButtonDisabled: PropTypes.bool.isRequired,
-//   handleChange: PropTypes.func.isRequired,
-// };
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Login;
