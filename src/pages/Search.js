db0349fed3ba2b81d4b48dcfb11469346ebe2ed6
luc0liv <luoliveiradacunha@gmail.com>
2022-11-04 16:47:27 -0300

    REQ 6: Listando albums pesquisados

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends Component {
   state = {
     artist: '',
     isButtonDisabled: true,
-    // loading: false,
+    albums: [],
+    loading: false,
+    message: '',
   };
 
   handleChange = ({ target }) => {
@@ -22,27 +26,68 @@ class Search extends Component {
     );
   };
 
+  searchAlbums = async (artist) => {
+    this.setState({
+      loading: true,
+    });
+    const reqAlbums = await searchAlbumsAPI(artist);
+    this.setState(
+      {
+        loading: false,
+        albums: reqAlbums,
+      },
+      () => {
+        this.setState((prevState) => ({
+          message: `Resultado de álbuns de: ${prevState.artist}`,
+          artist: '',
+        }));
+      },
+    );
+  };
+
   render() {
-    const { artist, isButtonDisabled } = this.state;
+    const { artist, isButtonDisabled, loading, albums, message } = this.state;
+    const loadingElement = <p>Carregando...</p>;
     return (
       <div>
         <Header />
         <h1 data-testid="page-search">Search</h1>
-        <form>
-          <input
-            placeholder="Nome do artista"
-            value={ artist }
-            onChange={ this.handleChange }
-            data-testid="search-artist-input"
-          />
-          <button
-            type="button"
-            data-testid="search-artist-button"
-            disabled={ isButtonDisabled }
-          >
-            Procurar
-          </button>
-        </form>
+        {loading ? (
+          loadingElement
+        ) : (
+          <form>
+            <input
+              placeholder="Nome do artista"
+              value={ artist }
+              onChange={ this.handleChange }
+              data-testid="search-artist-input"
+            />
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              disabled={ isButtonDisabled }
+              onClick={ () => this.searchAlbums(artist) }
+            >
+              Procurar
+            </button>
+          </form>
+        )}
+        {albums.length >= 1 ? (
+          <p>{message}</p>
+        ) : (
+          <p>Nenhum álbum foi encontrado</p>
+        )}
+        <ul>
+          {albums.map((album) => (
+            <Link
+              key={ album.collectionId }
+              to={ `album/${album.collectionId}` }
+              data-testid={ `link-to-album-${album.collectionId}` }
+            >
+              {album.collectionName}
+            </Link>
+          ))}
+        </ul>
       </div>
     );
   }
