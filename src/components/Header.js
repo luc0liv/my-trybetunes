5f109382636265e329644dd8593b8f453eaebc75
luc0liv <luoliveiradacunha@gmail.com>
2022-11-08 15:49:44 -0300

    Header estilizado

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Header extends Component {
   state = {
@@ -19,18 +20,43 @@ class Header extends Component {
 
   render() {
     const { loading, userName } = this.state;
+    const gradient = 'bg-gradient-to-r from-teaGreen to-blue via-fuchsia-300';
+    const headerStyle = `${gradient} p-6 text-darkPurple font-semibold`;
     return (
-      <header data-testid="header-component">
-        <nav>
-          <Link to="/search" data-testid="link-to-search">Search</Link>
-          <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
-          <Link to="/profile" data-testid="link-to-profile">Profile</Link>
+      <header
+        data-testid="header-component"
+        className={ `${headerStyle} shadow-sm` }
+      >
+        <nav className="flex flex-wrap items-center justify-center md:justify-between">
+          <div className="flex justify-between items-center w-full md:w-2/6">
+            <Link to="/search" data-testid="link-to-search" className="hover:opacity-60">
+              Search
+            </Link>
+            <Link
+              to="/favorites"
+              data-testid="link-to-favorites"
+              className="hover:opacity-60"
+            >
+              Favorites
+            </Link>
+            <Link
+              to="/profile"
+              data-testid="link-to-profile"
+              className="hover:opacity-60"
+            >
+              Profile
+            </Link>
+          </div>
+          <div>
+            {loading ? (
+              <Loading />
+            ) : (
+              <p data-testid="header-user-name" className="text-darkPurple text-sm">
+                {userName}
+              </p>
+            )}
+          </div>
         </nav>
-        {
-          loading
-            ? <p>Carregando...</p>
-            : <p data-testid="header-user-name">{ userName }</p>
-        }
       </header>
     );
   }
